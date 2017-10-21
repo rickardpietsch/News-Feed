5617e9b68e1b820a9bb5e7119c007492b24735d1
Rickard Pietsch <rickard.pietsch@gmail.com>
2017-10-21 21:57:32 +0200

    animation added for image

--- index.php
+++ index.php
@@ -12,54 +12,49 @@ require __DIR__.'/functions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Fysiken Crossfit Blog</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css">
-        <link rel="stylesheet" href="style.css">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Fysiken Crossfit Blog</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css">
+  <link rel="stylesheet" href="style.css">
 
-    </head>
-    <body>
-      <div class="wrapper">
+</head>
+<body>
+  <div class="wrapper">
 
-        <div class="top-nav">
-          <div class="left-nav">
-            <a href="https://www.facebook.com/FysikenCrossFit/">Facebook</a>
-            <a href="https://www.instagram.com/fysikencrossfit/">Instagram</a>
-          </div>
-
-          <div class="center-nav">
-            <h1>Fysiken Crossfit Blog</h1>
-          </div>
-
-          <div class="right-nav">
-            <a id="fysiken" href="https://www.fysiken.nu/">www.fysiken.nu</a>
-            <p id="telephone">Tel: 031-10 69 30</p>
-          </div>
-        </div>
-
-        <h2>An epic way to make your body a huge temple</h2>
-
-      <div class="danny-img">
-      <img class="image" src="images/danny.png" alt="dannybroflex">
+    <div class="top-nav">
+      <div class="left-nav">
+        <a href="https://www.facebook.com/FysikenCrossFit/">Facebook</a>
+        <a href="https://www.instagram.com/fysikencrossfit/">Instagram</a>
       </div>
+      <div class="center-nav">
+        <h1>Fysiken Crossfit Blog</h1>
+      </div>
+      <div class="right-nav">
+        <a id="fysiken" href="https://www.fysiken.nu/">www.fysiken.nu</a>
+        <p id="telephone">Tel: 031-10 69 30</p>
+      </div>
+    </div>
 
-      <?php foreach ($coaches as $coach): ?>
+    <div class="danny-img">
+      <img class="image" src="images/dannybro.png" alt="dannybroflex">
+    </div>
+
+    <h2>An epic way to make your body a huge temple</h2>
+
+    <?php foreach ($coaches as $coach): ?>
       <div class="container">
-          <div class="coach">
-            <div class="title"><h3><?php echo $coach['title']; ?></h3></div>
-            <div class="content"><p><?php echo $coach['content']; ?></p></div>
-            <div class="author"><h4><?php echo $coach['author']; ?></h4></div>
-            <div class="date"><?php echo $coach['date']; ?></div>
-            <div class="counter"><?php echo $coach['counter']; ?></div>
-          </div>
+        <div class="coach">
+          <div class="title"><h3><?php echo $coach['title']; ?></h3></div>
+          <div class="content"><p><?php echo $coach['content']; ?></p></div>
+          <div class="author"><h4><?php echo $coach['author']; ?></h4></div>
+          <div class="date"><?php echo $coach['date']; ?></div>
+          <div class="counter"><?php echo $coach['counter']; ?></div>
+        </div>
       </div>
-      <?php endforeach; ?>
+    <?php endforeach; ?>
 
-
-
-
-      </div>
-    </body>
+  </div>
+</body>
 </html>
