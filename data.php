b7a7f66c6104cf4f5cb1e742b55c3db4b4e71508
Rickard Pietsch <rickard.pietsch@gmail.com>
2017-10-30 13:32:58 +0100

    cleaned up code

--- data.php
+++ data.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-// Blog posts
+/* Blog posts! */
 $coaches = [
   [
     'id' => 1,
--- functions.php
+++ functions.php
@@ -2,31 +2,8 @@
 
 declare(strict_types=1);
 
-// foreach ($coaches as $coach) {
-//
-// }
-
-
-// require __DIR__.'/data.php';
-//
-// function sortByDate($a, $b) {
-//     return (int) $a['date'] > (int) $b['date'];
-// }
-//
-// usort($posts, 'sortByDate');
-//
-// foreach ($posts as $post) {
-//     echo $post['date'].'<br>';
-// }
-
 function sortByDate($a, $b) {
     return $a['date'] < $b['date'];
 }
 
 usort($coaches, 'sortByDate');
-
-// Funktion för att inte jämföra strängar? Edvin
-
-// foreach ($coaches as $coach) {
-//     echo $coach['date'].'<br>';
-// }
